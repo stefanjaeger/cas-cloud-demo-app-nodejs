d9a0055d4b204fa45f3ff4ce6701e95127ec1132
Stefan Jäger <sja@zuehlke.com>
2016-05-19 14:46:48 +0200

    updated text message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello CAS Cloud!');
 });
 
 app.listen(port, ipaddress, function () {
